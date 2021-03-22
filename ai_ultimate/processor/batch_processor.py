770c98b4eb2c0d3f86f92da9badfefa749428fa1
Pardeep Dogra <pardeepdgr@gmail.com>
2021-03-22 22:39:34 +0530

    cleanse tweets

--- ai_ultimate/processor/batch_processor.py
+++ ai_ultimate/processor/batch_processor.py
@@ -2,4 +2,12 @@ from pyspark.sql import DataFrame
 
 
 def process_batch(df: DataFrame, epoch_id: int):
-    pass
+    cleansed_tweets = process_tweet_batch(df)
+    print(cleansed_tweets)
+
+
+# TODO: what if tweets are so huge in number
+def process_tweet_batch(tweet_batch: DataFrame):
+    tweets = tweet_batch.collect()
+    extracted_tweets = [tweet.__getattr__("value") for tweet in tweets]
+    return extracted_tweets
